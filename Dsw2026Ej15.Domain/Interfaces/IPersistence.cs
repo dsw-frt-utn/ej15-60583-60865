8007aaa5d36f6d5d6fbe54565a51bf2aa9ddc3b4
Nicolas Rizza <nicolasrizza756@gmail.com>
2026-06-15 10:52:43 -0300

    creacion de IPersistence

---
+++ Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
@@ -0,0 +1,17 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistence
+    {
+        // Métodos para gestionar Doctores
+        List<Doctor> GetDoctors();
+        Doctor? GetDoctorById(Guid id);
+        void AddDoctor(Doctor doctor);
+
+        // Métodos para gestionar Especialidades
+        List<Speciality> GetSpecialities();
+        Speciality? GetSpecialityById(Guid id);
+        void AddSpeciality(Speciality speciality);
+    }
+}
